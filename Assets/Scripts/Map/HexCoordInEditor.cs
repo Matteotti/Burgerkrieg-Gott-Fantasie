b64febc82c4c7432da8457d146040c325c29efa4
ZJUSaltFIsh <2872528031@qq.com>
2022-11-08 21:34:28 +0800

    gtz 11.8 simple models added

--- Assets/Scripts/Map/HexCoordInEditor.cs
+++ Assets/Scripts/Map/HexCoordInEditor.cs
@@ -6,19 +6,20 @@ using UnityEngine;
 public class HexCoordInEditor : MonoBehaviour
 {
     public Inventory inventory;
-    private GameObject map;//the parent map(front or back)
+    [SerializeField]private GameObject map;//the parent map(front or back)
     public HexCell cell;
     
     void Start(){
         cell = GetComponent<HexCell>();
-        map = this.transform.parent.gameObject;//if wirtethis in Awake(), got an error...So it comes here
     }
 
     void Update(){
-        //move the cell when hexCoord is updated
-        float delX = inventory.hexEdgeLength * cell.hexCoord.x * Mathf.Sqrt(3) - inventory.hexEdgeLength * cell.hexCoord.y * Mathf.Sqrt(3)/2;
-        float delZ = inventory.hexEdgeLength * cell.hexCoord.y * 3/2;
-        Vector3 delTrans = new Vector3(delX, 0.0F, delZ);
-        this.transform.position = map.transform.position + delTrans;
+        if(this.transform.parent != null && inventory != null) {//move the cell when hexCoord is updated
+            map = this.transform.parent.gameObject;//if wirtethis in Awake(), got an error...So it comes here
+            float delX = inventory.hexEdgeLength * cell.hexCoord.x * Mathf.Sqrt(3) - inventory.hexEdgeLength * cell.hexCoord.y * Mathf.Sqrt(3)/2;
+            float delZ = inventory.hexEdgeLength * cell.hexCoord.y * 3/2;
+            Vector3 delTrans = new Vector3(delX, 0.0F, delZ);
+            this.transform.position = map.transform.position + delTrans;
+        }       
     }
 }
--- README.md
+++ README.md
@@ -1,22 +1,10 @@
 # Burgerkrieg-Gott-Fantasie
 Course project of ZJU Computer Game Programming Design :)
 
-version 2022.11.6.1, latest edit: gtz
+version 2022.11.8.2, latest edit: gtz
 
-新的框架
+加入了几个简单的白模(.fbx)，取代原本的精灵。
 
-现在修改六边形坐标会直接反映在世界坐标的变化
+模型挂载方式为在预制体下直接挂载整个导入的fbx（感觉这样子更好弄些？增加mesh组件的方法我还没搞明白）
 
-现在地图是躺着的了
-
-只会用预制体...
-
-精灵没有同步到新的框架（没时间写了），所以运行时什么都看不到
-
-HexCell - 对应原本的 MapGrid 和 MapGridBase
-
-HexCoord - 控制单元格大小和位置，单独摘出。和HexCell一起使用
-
-HexGrid- 对应原本的Map.cs。WARNING：这个没怎么修改，现在仅仅是能跑
-
-Maps - 对应原本的 MapManager.cs（担心重名所以改了名）。WARNING：同上
+模型切换还没有实现。
